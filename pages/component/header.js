b942aba98c99cbfb3bbe3f814f42e9bd202a0ebb
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2022-09-02 16:52:15 +1000

    added css and theme function to header but still bug on it. will be fixed soon.

--- pages/component/header.js
+++ pages/component/header.js
@@ -1,23 +1,58 @@
-import styles from "/styles/nav.module.scss"
-import { FaHome, FaRegSun } from "react-icons/fa"
-import Link from "next/link"
+import { useState, useEffect } from "react";
+import styles from "/styles/nav.module.scss";
+import { FaHome, FaGithubAlt, FaLinkedinIn, FaStaylinked } from "react-icons/fa";
+import { FiSun, FiMoon } from "react-icons/fi";
+import { ImBlog } from "react-icons/im";
+
+import Link from "next/link";
 
 // import styles from "/styles/Home.module.scss"
 export default function Header() {
-    return(
-        <header>
-            <nav className={styles.nav}>
-                <div className={styles.logo}>LOGO</div>
-                <div>
-                    <a className={styles.navMenu}><FaHome/>HOME</a>
-                    <Link href="/github">
-                        <a className={styles.navMenu}>WORK</a></Link>
-                    <a className={styles.navMenu}>POST</a>
-                </div>
-                <div className={styles.theme}>
-                    <FaRegSun/>
-                </div>
-            </nav>
-        </header>
-    )
+
+  let markup = <FiSun />
+  let themeClicked = false
+  const [theme, setData] = useState(markup);
+  
+  const themeHandler = () => {
+    console.log("TM",themeClicked)
+    themeClicked = !themeClicked
+    if(themeClicked) {
+      return <FiMoon />
+    } else {
+      return <FiSun />
+    }
+  }
+
+  return (
+    <header>
+      <nav className={styles.nav}>
+        <div className={styles.logo}>LOGO</div>
+        <div>
+          <a className={styles.navMenu}>
+            <FaHome className={styles.menuLogo}/>
+            HOME
+          </a>
+          <a className={styles.navMenu}>
+            <FaGithubAlt className={styles.menuLogo}/>
+            SOURCE
+          </a>
+          <a className={styles.navMenu}>
+            <FaLinkedinIn className={styles.menuLogo}/>
+            LINKED_IN
+          </a>
+          <a className={styles.navMenu}>
+            <ImBlog className={styles.menuLogo}/>
+            POST</a>
+        </div>
+        <div 
+          onClick={(e) => {
+            e.preventDefault()
+            setData(themeHandler)}}
+           
+          className={ themeClicked ?  styles.themeDark:styles.theme}>
+          {theme}
+        </div>
+      </nav>
+    </header>
+  );
 }
