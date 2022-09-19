595edc84acee39fbc898704ecbfbbb9ee176222e
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2022-09-19 15:56:28 +1000

    import workData in project component

--- components/project.js
+++ components/project.js
@@ -2,7 +2,7 @@ import styles from "/styles/components/project.module.scss";
 import globalStyles from "/styles/components/global_components/text.module.scss";
 // import { FaGithubSquare } from "react-icons/fa";
 // import { BiRocket } from "react-icons/Bi";
-// import workData from "./workData";
+import workData from "./workData";
 import Image from "next/image";
 import { useRef } from "react";
 import Link from "next/link";
