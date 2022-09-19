c74cd37f249fddcc45eb0d4fd5f4f63c6f4338a0
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2022-09-19 16:30:41 +1000

    add workData in project

--- components/project.js
+++ components/project.js
@@ -1,8 +1,8 @@
 import styles from "/styles/components/project.module.scss";
 import globalStyles from "/styles/components/global_components/text.module.scss";
-// import { FaGithubSquare } from "react-icons/fa";
-// import { BiRocket } from "react-icons/Bi";
-import projectData from "./workData";
+import { FaGithubSquare } from "react-icons/fa";
+import { BiRocket } from "react-icons/bi";
+// import workData from "./workData";
 import Image from "next/image";
 import { useRef } from "react";
 import Link from "next/link";
@@ -23,7 +23,28 @@ export default function Project({ hideTitle }) {
   const postClick = (obj) => {
     window.open(obj.postUrl);
   };
-  const workData = projectData()
+  const workData = [
+    {
+        "img": "/neko-japanese.png",
+        "alt": "image",
+        "title": "Neko-Japanese",
+        "description": "This is my first project built with Vue-CLI and Django, might be published as a product.",
+        "productDescription" : "Neko Japanese is a platform where learn Japanese, test and evaluate japanese knowledge, access viewable weakness and strong point in each component, community to discuss Japanese topics. Currently, this is for portfolio purpose, but might be published as a product.",
+        "githubUrl": "https://github.com/Goaty-yagi/quiz_project",
+        "postUrl": "https://neko-japanese.herokuapp.com/"
+    },
+    {
+        "img": "/quiz-ranking-dojo.png",
+        "alt": "image",
+        "title": "Quiz-Ranking-Dojo",
+        "description": `This is my final project in General Assembly development course.
+        I built SPA from scratch to understand how SPA works, I believe that this experience definitely help my future-self
+        when I learn new frameworks or libraries.
+        `,
+        "githubUrl": "https://github.com/Goaty-yagi/GA_SPA_Final_Project",
+        "postUrl": "https://quiz-ranking-dojo.herokuapp.com/"
+    }
+]
   const markup = workData.projectData.map((obj, index) => {
     return (
       <div className={styles.card} key={index}>
