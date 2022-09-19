01e5b5367c4501f3ab0235ac41b6b8681089d7c3
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2022-09-19 15:03:34 +1000

    remove workdata for test purpose

--- components/project.js
+++ components/project.js
@@ -2,7 +2,7 @@ import styles from "/styles/components/project.module.scss";
 import globalStyles from "/styles/components/global_components/text.module.scss";
 import { FaGithubSquare } from "react-icons/fa";
 import { BiRocket } from "react-icons/Bi";
-import workData from "./workData";
+// import workData from "./workData";
 import Image from "next/image";
 import { useRef } from "react";
 import Link from "next/link";
@@ -23,42 +23,42 @@ export default function Project({ hideTitle }) {
   const postClick = (obj) => {
     window.open(obj.postUrl);
   };
-  const markup = workData.projectData.map((obj, index) => {
-    return (
-      <div className={styles.card} key={index}>
-        {/* <Link href={"projects/" + obj.title} scroll={false} key={index}> */}
+  // const markup = workData.projectData.map((obj, index) => {
+  //   return (
+  //     <div className={styles.card} key={index}>
+  //       {/* <Link href={"projects/" + obj.title} scroll={false} key={index}> */}
 
-        <h3 className={styles.projectTitle}>{obj.title}</h3>
-        <div className={styles.imageWrapper}>
-          <Image
-            src={obj.img}
-            alt={obj.alt}
-            // className={styles.img}
-            layout="fill"
-            objectFit="cover"
-            objectPosition="50% 0"
-            // width="280px"
-            // height="150px"
-          ></Image>
-        </div>
-        <div className={styles.articleWrapper}>
-          <article>{obj.description}</article>
-        </div>
-        <div className={styles.iconWrapper}>
-          <FaGithubSquare
-            className={styles.gitIcon}
-            onClick={() => gitClick(obj)}
-          />
-          <BiRocket className={styles.gitIcon} onClick={() => postClick(obj)} />
-        </div>
-        {/* </Link> */}
-      </div>
-    );
-  });
+  //       <h3 className={styles.projectTitle}>{obj.title}</h3>
+  //       <div className={styles.imageWrapper}>
+  //         <Image
+  //           src={obj.img}
+  //           alt={obj.alt}
+  //           // className={styles.img}
+  //           layout="fill"
+  //           objectFit="cover"
+  //           objectPosition="50% 0"
+  //           // width="280px"
+  //           // height="150px"
+  //         ></Image>
+  //       </div>
+  //       <div className={styles.articleWrapper}>
+  //         <article>{obj.description}</article>
+  //       </div>
+  //       <div className={styles.iconWrapper}>
+  //         <FaGithubSquare
+  //           className={styles.gitIcon}
+  //           onClick={() => gitClick(obj)}
+  //         />
+  //         <BiRocket className={styles.gitIcon} onClick={() => postClick(obj)} />
+  //       </div>
+  //       {/* </Link> */}
+  //     </div>
+  //   );
+  // });
   return (
     <section>
       {!hideTitle && <h1 className={globalStyles.sectionH1}>Project</h1>}
-      <div className={styles.cardWrapper}>{markup}</div>
+      {/* <div className={styles.cardWrapper}>{markup}</div> */}
     </section>
   );
 }
