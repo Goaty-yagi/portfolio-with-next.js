4a0963ca1c64147431ef6b20d80c31acf2894a2e
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2022-09-28 14:56:56 +1000

    delete style in pages/project

--- pages/project.js
+++ pages/project.js
@@ -2,9 +2,9 @@ import Projects from "../components/project"
 
 export default function Project() {
     return (
-      <div className={styles.projectWrapper}>
-      <h1 className={styles.mainTitle}>PROJECT</h1>
-      <Projects hideTitle={true}/>
+      <div >
+      <h1 >PROJECT</h1>
+      <Projects/>
       </div>
       
     );
