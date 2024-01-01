186bb0e0e1a0c3f6d7b747e70c040c3e19caf2d2
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-01 17:08:51 +1100

    refactor: divide previous_notice from posts

--- components/posts/index.js
+++ components/posts/index.js
@@ -2,4 +2,5 @@ export { default as Dates } from "./dates";
 export { default as Tags } from "./tags";
 export { default as Git } from "./git";
 export { default as Thumbnail } from "./thumbnail";
+export { default as PreviousNotice } from "./previousNotice";
 
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -4,7 +4,6 @@ import matter from "gray-matter";
 import { marked } from "marked";
 import { TbArrowBigLeftLine, TbArrowBigRightLine } from "react-icons/tb";
 
-import { FcLink } from "react-icons/fc";
 import prismjs from "prismjs";
 import "prismjs/themes/prism-tomorrow.css";
 import "prismjs/components/prism-python.min";
@@ -13,7 +12,13 @@ import "prismjs/components/prism-bash.min";
 import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import CustomLink from "../../components/customLink";
-import { Tags, Dates, Git, Thumbnail } from "../../components/posts";
+import {
+  Tags,
+  Dates,
+  Git,
+  Thumbnail,
+  PreviousNotice,
+} from "../../components/posts";
 
 export default function PostPage({
   frontmatter: {
@@ -74,37 +79,7 @@ export default function PostPage({
         <Tags tags={tags} />
         {git && <Git git={git} />}
         <Thumbnail cover_image={cover_image} alt={alt} />
-        {previous && (
-          <>
-            <Box
-              textAlign={"center"}
-              fontSize={"1.3rem"}
-              whiteSpace={"pre-wrap"}
-            >
-              <span
-                style={{
-                  border: "1px solid #767717",
-                  borderRadius: "0.5rem",
-                  padding: "0.3rem 0.5rem",
-                  display: "inline-block",
-                }}
-              >
-                <Flex alignItems={"center"}>
-                  <FcLink />
-                  <Text ml={"0.5rem"}>In continuation from </Text>
-                  <Box
-                    className={"customA"}
-                    style={{ display: "inline-block" }}
-                  >
-                    <CustomLink href={origin + previous} scroll={true}>
-                      this article
-                    </CustomLink>
-                  </Box>
-                </Flex>
-              </span>
-            </Box>
-          </>
-        )}
+        {previous && <PreviousNotice previous={previous} />}
         {update_excerpt && (
           <Flex
             flexDirection={"column"}
